a4123e16b02c7199e010f531986a3071eeb4db54
Edward Kozlowski <ekozlowski1@gmail.com>
2016-03-13 09:53:27 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Edward on 3/13/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println();
+    }
+}
